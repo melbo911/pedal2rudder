04a02cc863a049b9ccbb84031657ce516b9d2762
Melbo <melbo@jnet.ath.cx>
2021-01-01 23:35:26 +0100

    changes

--- src/pedal2rudder.lua
+++ src/pedal2rudder.lua
@@ -1,8 +1,6 @@
 --
 -- left/right racing pedals to rudder/heading mapper
 --
--- version 1.1 - 20210101 - melbo @x-plane.org
---
 
 -- axis types
 -- 1 pitch
@@ -17,19 +15,18 @@ local use_brakes = 0    -- set to 0 when using helicopters with skids
 local left_axis  = 0
 local right_axis = 0
 
-local axis_type = dataref_table("sim/joystick/joystick_axis_assignments")
-local axis_values = dataref_table("sim/joystick/joystick_axis_values")
-local brake = dataref_table("sim/flightmodel/controls/parkbrake")
-local rudder = dataref_table("sim/joystick/yoke_heading_ratio")
-
 -- functions start here ------------------------
 
 function readAxisTypes()
+  local axis_type
+
   for i = 0, 499 do
-    if axis_type[i] == 6 then     -- left brake pedal ?
+    -- get axis types 
+    axis_type = get( "sim/joystick/joystick_axis_assignments", i )
+    if axis_type == 6 then     -- left brake pedal ?
       left_axis = i
     else
-      if axis_type[i] == 7 then   -- right brake pedal ?
+      if axis_type == 7 then   -- right brake pedal ?
         right_axis = i
       end
     end 
@@ -37,11 +34,11 @@ function readAxisTypes()
 end
 
 function readPedals()
-  local Diff = axis_values[left_axis] - axis_values[right_axis]
-  if rudder[0] ~= Diff then
-    rudder[0] = Diff
+  local Diff = left_brake - right_brake
+  if rudder ~= Diff then
+    rudder = Diff
     if use_brakes == 0 then
-      brake[0] = 0.5       -- do not release breaks
+      brake = 0.5       -- do not release breaks
     end
   end
 end
@@ -53,7 +50,12 @@ readAxisTypes()
 if left_axis == 0 or right_axis == 0 then
   XPLMSpeakString("no brake axis defined") 
 else
-  -- XPLMSpeakString("brake axis mapped to rudder") 
+  DataRef("brake", "sim/flightmodel/controls/parkbrake", "writeable")
+  DataRef("rudder", "sim/joystick/yoke_heading_ratio", "writable")
+  DataRef("left_brake", "sim/joystick/joystick_axis_values", "readonly", left_axis)
+  DataRef("right_brake", "sim/joystick/joystick_axis_values", "readonly", right_axis)
+  XPLMSpeakString("brake axis mapped to rudder") 
+
   do_every_frame("readPedals()")
 end
 
